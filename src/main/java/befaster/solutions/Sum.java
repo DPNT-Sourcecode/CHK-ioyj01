f658c747dc0d47be6ea48a2f9b02758d203f1eea
sbx_user1059 <sbx_user1059@ip-10-12-226-158.eu-west-2.compute.internal>
2018-04-16 19:11:06 +0000

    Mon Apr 16 19:11:06 UTC 2018

--- src/main/java/befaster/solutions/Sum.java
+++ src/main/java/befaster/solutions/Sum.java
@@ -1,7 +1,5 @@
 package befaster.solutions;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class Sum {
 
     public static int sum(int x, int y) {
--- src/test/java/befaster/solutions/SumTest.java
+++ src/test/java/befaster/solutions/SumTest.java
@@ -2,38 +2,40 @@ package befaster.solutions;
 
 import org.junit.Test;
 
+import static befaster.solutions.Sum.sum;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class SumTest {
 
-    public static final int ONE_MORE_THAN_MAX = 101;
-    public static final int ONE_LESS_THAN_MIN = -1;
-    public static final int MIN_ALLOWED = 1;
+    private static final int ONE_MORE_THAN_MAX = 101;
+    private static final int ONE_LESS_THAN_MIN = -1;
+    private static final int MIN_ALLOWED = 1;
+    private static final int MAX_ALLOWED = 100;
 
     @Test
     public void givenIntegersInAllowedRangeSumReturnsTotal() {
-        assertThat(Sum.sum(1, 100), equalTo(101));
+        assertThat(sum(MIN_ALLOWED, MAX_ALLOWED), equalTo(101));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void givenFirstArgumentIsLessThanMinAllowedSumThrowsIllegalArgumentException() {
-        Sum.sum(ONE_LESS_THAN_MIN, MIN_ALLOWED);
+        sum(ONE_LESS_THAN_MIN, MIN_ALLOWED);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void givenSecondArgumentIsLessThanMinAllowedSumThrowsIllegalArgumentException() {
-        Sum.sum(MIN_ALLOWED, ONE_LESS_THAN_MIN);
+        sum(MIN_ALLOWED, ONE_LESS_THAN_MIN);
     }
 
 
     @Test(expected = IllegalArgumentException.class)
     public void givenFirstArgumentGreaterThanMaxAllowedSumThrowsIllegalArgumentException() {
-        Sum.sum(ONE_MORE_THAN_MAX, MIN_ALLOWED);
+        sum(ONE_MORE_THAN_MAX, MIN_ALLOWED);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void givenSecondArgumentGreaterThanMaxAllowedSumThrowsIllegalArgumentException() {
-        Sum.sum(MIN_ALLOWED, ONE_MORE_THAN_MAX);
+        sum(MIN_ALLOWED, ONE_MORE_THAN_MAX);
     }
 }
