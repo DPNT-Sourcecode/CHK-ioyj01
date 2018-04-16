bb6e9e045e9bff278315291951e639be40e72999
sbx_user1059 <sbx_user1059@ip-10-12-226-158.eu-west-2.compute.internal>
2018-04-16 19:05:06 +0000

    Mon Apr 16 19:05:06 UTC 2018

--- src/main/java/befaster/solutions/Sum.java
+++ src/main/java/befaster/solutions/Sum.java
@@ -5,7 +5,7 @@ import befaster.runner.SolutionNotImplementedException;
 public class Sum {
 
     public static int sum(int x, int y) {
-        if ( x < 0 || y < 0) {
+        if ( x < 0 || x > 100 || y < 0 || y > 100) {
             throw new IllegalArgumentException("Received negative integer, brief restricts this method to positive integers between 0-100");
         }
 
--- src/test/java/befaster/solutions/SumTest.java
+++ src/test/java/befaster/solutions/SumTest.java
@@ -7,29 +7,32 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class SumTest {
 
+    public static final int ONE_MORE_THAN_MAX = 101;
+    public static final int ONE_LESS_THAN_MIN = -1;
+
     @Test
     public void givenPositiveIntegersSumReturnsTotal() {
         assertThat(Sum.sum(1, 1), equalTo(2));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void givenFirstArgumentIsNegativeSumThrowsIllegalArgumentException() {
-        Sum.sum(-1, 1);
+    public void givenFirstArgumentIsLessThanMinAllowedSumThrowsIllegalArgumentException() {
+        Sum.sum(ONE_LESS_THAN_MIN, 1);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void givenSecondArgumentIsNegativeSumThrowsIllegalArgumentException() {
-        Sum.sum(1, -1);
+    public void givenSecondArgumentIsLessThanMinAllowedSumThrowsIllegalArgumentException() {
+        Sum.sum(1, ONE_LESS_THAN_MIN);
     }
 
 
     @Test(expected = IllegalArgumentException.class)
-    public void givenFirstArgumentGreaterThanMaxSumThrowsIllegalArgumentException() {
-        Sum.sum(101, 1);
+    public void givenFirstArgumentGreaterThanMaxAllowedSumThrowsIllegalArgumentException() {
+        Sum.sum(ONE_MORE_THAN_MAX, 1);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void givenSecondArgumentGreaterThanMaxSumThrowsIllegalArgumentException() {
-        Sum.sum(1, 101);
+    public void givenSecondArgumentGreaterThanMaxAllowedSumThrowsIllegalArgumentException() {
+        Sum.sum(1, ONE_MORE_THAN_MAX);
     }
 }
