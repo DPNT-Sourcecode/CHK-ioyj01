ff9fb3c5bc104d013480ff091aeae621517f2d5b
sbx_user1059 <sbx_user1059@ip-10-12-226-158.eu-west-2.compute.internal>
2018-04-16 19:15:18 +0000

    Mon Apr 16 19:15:18 UTC 2018

---
+++ src/test/java/befaster/solutions/HelloTest.java
@@ -0,0 +1,10 @@
+package befaster.solutions;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class HelloTest {
+
+}
