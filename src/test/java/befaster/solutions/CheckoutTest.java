1b4f87374bc785dd17b87968ae695e1ad785c036
sbx_user1059 <sbx_user1059@ip-10-11-134-83.eu-west-2.compute.internal>
2018-04-23 10:21:29 +0000

    Mon Apr 23 10:21:29 UTC 2018

---
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -0,0 +1,16 @@
+package befaster.solutions;
+
+import org.junit.Test;
+
+import static befaster.solutions.Sum.sum;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CheckoutTest {
+
+    @Test
+    public void givenOneItemCheckoutReturnsCorrectPrice() {
+
+    }
+
+}
