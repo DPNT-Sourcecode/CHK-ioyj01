8682350315ae1b15f931672b0c48802306c4aac2
sbx_user1059 <sbx_user1059@ip-10-11-134-83.eu-west-2.compute.internal>
2018-04-23 10:24:29 +0000

    Mon Apr 23 10:24:29 UTC 2018

--- src/test/java/befaster/solutions/CheckoutTest.java
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -6,6 +6,11 @@ import static befaster.solutions.Sum.sum;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class CheckoutTest {
 
     @Test
